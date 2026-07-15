2cbb44b6a356a296a99311207fc91379745fb4c5
yangliangquan <yangliangquan@163.com>
2026-07-15 11:34:56 +0800

    feat: add IO pins assignments and macros for GPIO configuration

--- os/hal/boards/CH32H417_EVT/board.h
+++ os/hal/boards/CH32H417_EVT/board.h
@@ -29,43 +29,6 @@
 
 #define CH32H417
 
-/*
- * IO pins assignments.
- */
-#define GPIO_PIN0              0U
-#define GPIO_PIN1              1U
-#define GPIO_PIN2              2U
-#define GPIO_PIN3              3U
-#define GPIO_PIN4              4U
-#define GPIO_PIN5              5U
-#define GPIO_PIN6              6U
-#define GPIO_PIN7              7U
-#define GPIO_PIN8              8U
-#define GPIO_PIN9              9U
-#define GPIO_PIN10             10U
-#define GPIO_PIN11             11U
-#define GPIO_PIN12             12U
-#define GPIO_PIN13             13U
-#define GPIO_PIN14             14U
-#define GPIO_PIN15             15U
-#define GPIO_PIN16             16U
-
-#define PIN_MODE_INPUT(n)           (0U << ((n) * 2U))
-#define PIN_MODE_OUTPUT(n)          (1U << ((n) * 2U))
-#define PIN_MODE_ALTERNATE(n)       (2U << ((n) * 2U))
-#define PIN_MODE_ANALOG(n)          (3U << ((n) * 2U))
-#define PIN_ODR_LOW(n)              (0U << (n))
-#define PIN_ODR_HIGH(n)             (1U << (n))
-#define PIN_OTYPE_PUSHPULL(n)       (0U << (n))
-#define PIN_OTYPE_OPENDRAIN(n)      (1U << (n))
-#define PIN_OSPEED_VERYLOW(n)       (0U << ((n) * 2U))
-#define PIN_OSPEED_LOW(n)           (1U << ((n) * 2U))
-#define PIN_OSPEED_MEDIUM(n)        (2U << ((n) * 2U))
-#define PIN_OSPEED_HIGH(n)          (3U << ((n) * 2U))
-#define PIN_PUPDR_FLOATING(n)       (0U << ((n) * 2U))
-#define PIN_PUPDR_PULLUP(n)         (1U << ((n) * 2U))
-#define PIN_PUPDR_PULLDOWN(n)       (2U << ((n) * 2U))
-
 #if !defined(_FROM_ASM_)
 #ifdef __cplusplus
 extern "C" {
